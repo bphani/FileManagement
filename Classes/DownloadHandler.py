fd65a5afa207a26d1647fc6081265043f6108bf1
aminjamal <aminjamal10@gmail.com>
2019-09-30 18:10:14 +0430

    morning push :,(

--- Classes/DownloadHandler.py
+++ Classes/DownloadHandler.py
@@ -2,11 +2,15 @@ from DataBase.DB import files_collection
 from bson.objectid import ObjectId
 from starlette.responses import FileResponse
 from Classes.FileActionTools import FileTools
+from Classes.Tools import Tools
 
 
 class DownloadHandler:
     @staticmethod
     def download(file_id):
-        file = files_collection.find_one({'_id': ObjectId(file_id)}, {"Name", "MimeType"})
-        path = FileTools.save_file_path(file["Name"])
-        return FileResponse(path, media_type=file['MimeType'])
+        try:
+            file = files_collection.find_one({'_id': ObjectId(file_id)}, {"Name", "MimeType"})
+            path = FileTools.save_file_path(file["Name"])
+            return FileResponse(path, media_type=file['MimeType'])
+        except Exception as ex:
+            return Tools.Result(False,ex.args)
