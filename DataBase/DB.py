30e6359b5cac2ff995d6464765300639cd829228
aminjamal <aminjamal10@gmail.com>
2019-09-30 17:46:20 +0430

    upload mechanism

--- DataBase/DB.py
+++ DataBase/DB.py
@@ -1,19 +1,37 @@
 import pymongo
 import json
 import os
-import logging
+import requests
+from Classes.Tools import Tools
 
+Decode = Tools.decode
+Initer = Tools.initer
 
-with open('./Configs/Configs.json') as f:
+dirpath = os.getcwd()
+
+DEBUG = False
+with open(os.path.join(dirpath, "./Configs/Configs.json")) as f:
     configs = json.load(f)
-logging.warning(f"Getting database string ...")
 
-#database_url = os.environ["MongoDbUrl"]
-# if database_url is None:
-database_url = "mongodb://localhost:27017"
-logging.warning(f"Database Url : {database_url}")
+if not DEBUG:
+
+    get_database_url = json.loads(
+        requests.get("{0}/Services/config/get/FileManagement".format(configs['MicroServiceManagementURl']),
+                     verify=False).content)
+
+    if not get_database_url["State"]:
+        exit(1)
+
+    get_database_url = json.loads(get_database_url["Description"])
+
+    url = Decode(get_database_url["DatabaseString"],get_database_url["Key"][2:-1])
+    print(url)
+
+else:
+    url = 'mongodb://localhost:27017'
 
 
-mongodb = pymongo.MongoClient(database_url)
+
+mongodb = pymongo.MongoClient(url)
 database = mongodb[configs["Database"]["Name"]]
 files_collection = database[configs["Database"]["Collections"]["Files"]]
---
+++ Dockerfile
@@ -0,0 +1,50 @@
+FROM ubuntu:18.04
+
+EXPOSE 3005
+
+RUN apt-get update -y && \
+    apt-get install -y python3 python3-dev python3-pip
+
+
+RUN apt-get install -y sudo curl wget locales
+RUN locale-gen en_CA.UTF-8
+ENV LC_ALL=en_CA.UTF-8
+ENV LANG=en_CA.UTF-8
+ENV LANGUAGE=en_CA.UTF-8
+
+# We copy just the requirements.txt first to leverage Docker cache
+COPY ./requirements.txt ./app/requirements.txt
+
+COPY . ./app
+
+
+
+WORKDIR /app
+
+
+RUN pip3  install -r requirements.txt
+RUN pip3 install uvicorn
+RUN pip3 install fastapi
+RUN pip3 install fastapi[all]
+RUN pip3 install email-validator
+RUN pip3 install pymongo
+
+
+
+
+
+#CMD ["python3","-u","app.py"]
+#CMD ["/usr/local/bin/gunicorn", "--config", "gunicorn_config.py" , "app:app","uvicorn.workers.UvicornWorker"]
+#CMD ["/usr/local/bin/uvicorn","--host","0.0.0.0" ,"app:app","--port","3005"]
+CMD ["uvicorn","--host","0.0.0.0" ,"app:app","--port","3005"]
+
+#FROM tiangolo/uvicorn-gunicorn:python3.7
+#EXPOSE 80
+#RUN pip3 install fastapi
+#RUN pip3 install fastapi[all]
+#RUN pip3 install email-validator
+#RUN pip3 install pymongo
+#
+#COPY . /app
+#WORKDIR /app
+#RUN pip  install -r requirements.txt
