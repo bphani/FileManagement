f53a08f7f8f2fc0cb09e014b00ec52995afdd307
aminjamal <aminjamal10@gmail.com>
2019-09-30 14:25:56 +0430

    upload mechanism

--- Classes/UploadHandler.py
+++ Classes/UploadHandler.py
@@ -8,11 +8,11 @@ from Classes.FileActionTools import FileTools
 
 class UploadHandler:
     @staticmethod
-    def upload_file(file: UploadFile, file_extension, permission_level, uploader_id, file_size, category):
+    def upload_file(file: UploadFile, mime_type, permission_level, uploader_id, file_size, category):
         # create safe and unique file name
-        new_file_name = FileTools.file_name_generator(file_extension)
+        new_file_name = FileTools.file_name_generator(mime_type)
         # initialize file's model
-        file_for_db = Object(Name=new_file_name, FileSize=int(file_size), FileExtension=file_extension,
+        file_for_db = Object(Name=new_file_name, FileSize=int(file_size), MimeType=mime_type,
                              PermissionLevel=permission_level, UploaderId=uploader_id,
                              Category=category, Status="Uploaded")
         # save file
@@ -20,5 +20,5 @@ class UploadHandler:
 
         # insert model to database
         item = files_collection.insert_one(file_for_db.__dict__)
-
+        # return id of uploaded file for download
         return {"Id": str(item.inserted_id)}
