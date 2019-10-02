e16e8bf95e54b162211dd722ce2c4a3b5e285303
aminjamal <aminjamal10@gmail.com>
2019-10-02 14:06:51 +0430

    reformat

--- API/UploadApi.py
+++ API/UploadApi.py
@@ -1,9 +1,8 @@
-from fastapi import APIRouter, File, UploadFile, Form, Header,Depends
+from fastapi import APIRouter, File, UploadFile, Form, Header, Depends
 from Classes.UploadHandler import UploadHandler
 from Models.ResponseModels import UploadFileResponse
-from Authentications.Authentication import is_login , get_token_header
+from Authentications.Authentication import is_login, get_token_header
 from starlette.requests import Request
-
 
 upload_route = APIRouter()
 
@@ -12,9 +11,7 @@ upload_route = APIRouter()
 # todo : check user permission for uploading form server
 
 @upload_route.post('/upload/data-form/', dependencies=[Depends(is_login)],
-                                         tags=["UPLOAD"],
-                                         response_model=UploadFileResponse)
-async def upload_file_form_data(request:Request,file: UploadFile = File(...)
-                                ,PermissionLevel: str = Form(...)):
-
-    return UploadHandler.upload_file(file, file.content_type, PermissionLevel,request.headers["Id"], 0, "ProductImage")
+                   tags=["UPLOAD"],
+                   response_model=UploadFileResponse)
+async def upload_file_form_data(request: Request, file: UploadFile = File(...), PermissionLevel: str = Form(...)):
+    return UploadHandler.upload_file(file, file.content_type, PermissionLevel, request.headers["Id"], 0, "ProductImage")
