e00ed745d319b4faf9ccde7a63a7ef47bf9c8a6b
aminjamal <aminjamal10@gmail.com>
2019-10-22 14:33:54 +0430

    update classes

--- Test/main.py
+++ Test/main.py
@@ -10,10 +10,10 @@ print(result)
 
 
 def upload_file():
-    a = requests.post("http://localhost:3005/upload/data-form/",data={
+    a = requests.post("http://chichiapp.ir:3005/upload/data-form/",data={
         "PermissionLevel":"Public"
 
-    },files={"file":open("./1.png",'rb')},headers={
+    },files={"file":open("./1.jpg",'rb')},headers={
         "Id":"5d87e194549ae0267b5268cc",
         "Token":"6109bfa925d615dc888c94d1ba858bad960f3dcb95a69453bd6dd1ba8acc4c49"
     }).content
